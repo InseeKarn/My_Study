a1b078959b3558c87de7a2bb3c5c34c9e9475e63
Karnphong Saratontiphong <inseekan@gmail.com>
2025-09-17 12:48:45 +0700

    Calculator

---
+++ C/Calculator.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <math.h>
+
+void Calculator(int a,int b, char op){
+    
+    
+    switch(op){
+        case '+':
+            printf("%d + %d = %d",a,b, a+b);
+            break;
+        case '-':
+            printf("%d - %d = %d",a,b, a-b);
+            break;
+        case '*':
+            printf("%d * %d = %d",a,b, a*b);
+            break;
+        case '/':
+            if(b!=0){
+                printf("%d / %d = %d",a,b, a/b);
+            }
+            else{
+                printf("Error: Division by zero");
+            }
+            break;
+        case 's':
+            if(a>=0){
+                printf("sqrt(%d) = %.2f",a, sqrt(a));
+            }
+            else{
+                printf("Error: Square root of negative number");
+            }
+            break;
+    }
+}
+
+int main(){
+    int a,b;
+    char op;
+    printf("Enter 1st number: ");
+    scanf("%d", &a);
+    printf("Enter an operator (+, -, *, /, ^, s): ");
+    scanf(" %c", &op);
+    if(op!='s'){
+        printf("Enter 2nd number: ");
+        scanf("%d", &b);
+    }
+    Calculator(a,b,op);
+
+    return 0;
+}
--- C/tempCodeRunnerFile.c
+++
@@ -1,4 +0,0 @@
-    printf("%d,%d;\n",a,b);
-    *a = *a + *b;
-    *b = *b + 1;
-    printf("%d,%d;\n",a,b);
