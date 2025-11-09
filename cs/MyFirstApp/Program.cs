25133df6a7c46b29969ff3b123651b81f3069393
Karnphong Saratontiphong <inseekan@gmail.com>
2025-11-09 21:13:28 +0700

    Csharp

---
+++ cs/MyFirstApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public static class Calculator
+{
+    static void Main()
+    {
+
+        string data = "";
+        Console.WriteLine("Enter Number and OP for cal: ");
+        data = Console.ReadLine();
+
+        char[] OP = { '+', '-', 'x', '/' };
+        
+        string[] splitData = data.Split(OP);
+
+        double[] Numbers = new double[splitData.Length];
+
+        for (int i = 0; i < splitData.Length; i++)
+        {
+            if (double.TryParse(splitData[i], out double result))
+            {
+                Numbers[i] = result;
+            }
+            else
+            {
+                Console.WriteLine($"Invalid number: {splitData[i]}");
+                return;
+            }
+        }
+        foreach (double num in Numbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
+
